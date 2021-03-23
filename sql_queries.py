652c5e478319ba265b1ed63a513fca04be1b3b8c
Ans Elbousiffi <AnsEMU@users.noreply.github.com>
2021-03-24 00:17:40 +0200

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -65,32 +65,32 @@ time_table_create = ("""
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays  
-   (start_time, userId, level, song_id, artist_id, sessionId, location, userAgent) 
-   VALUES (%s, %s, %s, %s, %s, %s, %s, %s) 
+songplay_table_insert = ("""INSERT INTO songplays
+   (start_time, userId, level, song_id, artist_id, sessionId, location,
+   userAgent)
+   VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
    ON CONFLICT DO NOTHING;""")
 
 user_table_insert = ("""
-  INSERT INTO users 
-  (userId ,firstName ,lastName ,gender , level) 
-  VALUES (%s, %s, %s, %s, %s) 
+  INSERT INTO users
+  (userId ,firstName ,lastName ,gender , level)
+  VALUES (%s, %s, %s, %s, %s)
   ON CONFLICT (userId) DO UPDATE SET level = excluded.level;""")
-
 song_table_insert = ("""
-  INSERT INTO songs 
-  (song_id ,title ,artist_id ,year ,duration) 
-  VALUES (%s, %s, %s, %s, %s) 
+  INSERT INTO songs
+  (song_id ,title ,artist_id ,year ,duration)
+  VALUES (%s, %s, %s, %s, %s)
   ON CONFLICT (song_id) DO NOTHING;""")
 
 artist_table_insert = ("""
-  INSERT INTO artists 
-  (artist_id ,artist_name ,artist_location ,artist_latitude ,artist_longitude) 
-  VALUES (%s, %s, %s, %s, %s) 
+  INSERT INTO artists
+  (artist_id ,artist_name ,artist_location ,artist_latitude ,artist_longitude)
+  VALUES (%s, %s, %s, %s, %s)
   ON CONFLICT (artist_id) DO NOTHING;""")
 
 time_table_insert = ("""
-  INSERT INTO time 
-  (start_time ,hour ,day ,week ,month ,year ,weekday) 
+  INSERT INTO time
+  (start_time ,hour ,day ,week ,month ,year ,weekday)
   VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING;""")
 
 # FIND SONGS
@@ -105,5 +105,8 @@ song_select = ("""select s.song_id, a.artist_id
 """)
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create, user_table_create,
+                        song_table_create, artist_table_create,
+                        time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop,
+                      artist_table_drop, time_table_drop]
