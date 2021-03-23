61020d16613be898d5de20a0431e408be93ada32
Ans Elbousiffi <AnsEMU@users.noreply.github.com>
2021-03-23 15:55:03 +0200

    Update etl.py

--- etl.py
+++ etl.py
@@ -8,9 +8,12 @@ from datetime import datetime
 
 def process_song_file(cur, filepath):
     """
-    In this process a song data file is being loaded from a filepath which has been given as an arugment.
-    It pull out a certin information from song data in order to store it into the songs table.
-    Then it pull out the certin information from song data in order to store it into the artists table.
+    In this process a song data file is being loaded from a filepath
+    which has been given as an arugment.It pull out a certin
+    information from song data in order to store it
+    into the songs table.
+    Then it pull out the certin information
+    from song data in order to store it into the artists table.
 
     INPUTS:
     * cur the cursor variable
@@ -20,19 +23,22 @@ def process_song_file(cur, filepath):
     df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = df[['song_id','title','artist_id','year','duration']].values[0].tolist()
+    song_data = df[['song_id', 'title', 'artist_id', 'year',
+                    'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
-    
     # insert artist record
-    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0].tolist()
+    artist_data = df[['artist_id', 'artist_name', 'artist_location',
+                      'artist_latitude',
+                      'artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     """
-    In this process a log data file is being loaded from a filepath which has been given as an arugment.
-    It pull out a certin information from log data in order to store it into the time, users and sonsplays tables.
-    
+    In this process a log data file is being loaded from
+    a filepath which has been given as an arugment.
+    It pull out a certin information from log data
+    in order to store it into the time, users and sonsplays tables.
     INPUTS:
     * cur the cursor variable
     * filepath the file path to the log data file
@@ -41,23 +47,21 @@ def process_log_file(cur, filepath):
     df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = df[df['page']=='NextSong']
+    df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = pd.to_datetime(df['ts'],unit='ms')
-    
+    t = pd.to_datetime(df['ts'], unit='ms')
     # insert time data records
-    time_data = [df['ts'], t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday ]
-    column_labels = ['start_time', 'hour', 'day','week', 'month', 'year', 'weekday']
-    time_df = pd.DataFrame(
-    dict(zip(column_labels, time_data))
-    )
-
+    time_data = [df['ts'], t.dt.hour, t.dt.day, t.dt.week, t.dt.month,
+                 t.dt.year, t.dt.weekday]
+    column_labels = ['start_time', 'hour', 'day', 'week', 'month', 'year',
+                     'weekday']
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = df[['userId','firstName','lastName','gender','level']]
+    user_df = df[['userId',  'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -65,27 +69,27 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
-        # insert songplay record
-        songplay_data = [row['ts'], row['userId'], row['level'], songid, artistid, row['sessionId'], row['location'], row['userAgent']]
-        cur.execute(songplay_table_insert, songplay_data)
+    # insert songplay record
+    songplay_data = [row['ts'], row['userId'], row['level'], songid,
+                     artistid, row['sessionId'], row['location'],
+                     row['userAgent']]
+    cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
@@ -100,7 +104,9 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student"
+        )
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
